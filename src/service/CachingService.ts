bdb3d460f10160ba7a009e61c36314b67f429ca3
Hrvoje Kasabasic <hrvoje.kasabasic@sedamIT.hr>
2017-12-03 22:31:55 +0100

    feat: group local storage caching functs into a single adapter object

--- src/service/CachingService.ts
+++ src/service/CachingService.ts
@@ -1,27 +1,40 @@
 
+const LocalStorageAdapter = {
+
+	readCache<T>(key : string) : string | null {
+		return localStorage.getItem(key);
+	},
+
+	writeCache(key : string, value : string) {
+		localStorage.setItem(key, value);
+	},
+
+	clearCache(key : string) {
+		localStorage.removeItem(key);
+	}
+
+};
+
+
+/**
+ * Simple key-value caching service that can use arbitrary method for persisting values.
+ * Currently, values are persited in localStorage.
+ */
 export class CachingService {
+	private static adapter = LocalStorageAdapter;
 
 	static readKey<T>(key : string) : T {
-		let val = readCache(key);
+		let val = this.adapter.readCache(key);
 		return val != null ? JSON.parse(val) : val;
 	}
 
 	static setKey<T>(key : string, value : T) {
-		writeCache(key, JSON.stringify(value));
+		this.adapter.writeCache(key, JSON.stringify(value));
 	}
 
 	static clearKey(key : string) : void {
-		clearCache(key);
+		this.adapter.clearCache(key);
 	}
 
 }
 
-function readCache<T>(key : string) : string | null {
-	return localStorage.getItem(key);
-}
-function writeCache(key : string, value : string) {
-	localStorage.setItem(key, value);
-}
-function clearCache(key : string) {
-	localStorage.removeItem(key);
-}
